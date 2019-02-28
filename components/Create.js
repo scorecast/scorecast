314037231d20889e2f4695880d10e3cb57fa4678
skipkayhil <skipkayhil@gmail.com>
2019-02-27 20:48:13 -0500

    connect FireStore to Create

--- components/Create.js
+++ components/Create.js
@@ -1,67 +1,77 @@
-import {Component} from "react";
+import React, { Component } from 'react';
 import {
     View,
     Text,
     StyleSheet,
     ListView,
-    TouchableOpacity
-} from "react-native";
-import React from "react";
+    TouchableOpacity,
+} from 'react-native';
 
 import Icon from 'react-native-vector-icons/FontAwesome';
 import { NativeRouter, Route, Link } from 'react-router-native';
 import { firebase } from '../config/store';
 
-import {styles, pallette} from '../styles';
+import { firestoreConnect } from 'react-redux-firebase';
 
-export default class Join extends Component {
+import { styles, pallette } from '../styles';
+import { connect } from 'react-redux';
+import { compose } from 'redux';
+
+class Create extends Component {
     constructor(props) {
         super(props);
         this.state = {
             selected: 0,
-            templates: [],
         };
     }
 
     componentDidMount() {
-        firebase
-            .firestore()
-            .collection('templates')
-            .get()
-            .then(templates => {
-                templates.forEach(template => {
-                    this.setState({
-                        templates: [...this.state.templates, template],
-                    });
-                });
-            })
-            .catch(function(error) {
-                console.log('Error getting document:', error);
-            });
+        // firebase
+        //     .firestore()
+        //     .collection('templates')
+        //     .get()
+        //     .then(templates => {
+        //         templates.forEach(template => {
+        //             this.setState({
+        //                 templates: [...this.state.templates, template],
+        //             });
+        //         });
+        //     })
+        //     .catch(function(error) {
+        //         console.log('Error getting document:', error);
+        //     });
     }
 
     render() {
         let dataSource = new ListView.DataSource({
             rowHasChanged: (r1, r2) => r1 !== r2,
-        }).cloneWithRows(this.state.templates);
+        }).cloneWithRows(this.props.templates || []);
 
-        var options =["Home","Savings","Car","GirlFriend"];
+        var options = ['Home', 'Savings', 'Car', 'GirlFriend'];
 
         return (
             <View style={styles.content}>
                 <Text style={styles.header}>Create</Text>
-                <ListView dataSource={dataSource}
-                          style={{ flex: 1 }}
-                          enableEmptySections={true}
-                          renderRow={(template) => (
-                              <Link to="/discover">
-
-                                      <Text>{template.get('name')}</Text>
-
-                              </Link>)}/>
-            </View>);
+                <ListView
+                    dataSource={dataSource}
+                    style={{ flex: 1 }}
+                    enableEmptySections={true}
+                    renderRow={template => (
+                        <Link to="/discover">
+                            <Text>{template.name}</Text>
+                        </Link>
+                    )}
+                />
+            </View>
+        );
     }
 }
 
-const localStyles = StyleSheet.create({
-});
+const localStyles = StyleSheet.create({});
+
+export default compose(
+    firestoreConnect(['templates']),
+    connect((state, props) => ({
+        templates: state.firestore.ordered.templates,
+    }))
+)(Create);
