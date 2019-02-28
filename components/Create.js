19a322884da19058f98a4d084c38b4d6b2205c34
Jack Bonaguro <jbonaguro2@gmail.com>
2019-02-27 20:39:18 -0500

    Making nested router for create

--- components/Create.js
+++ components/Create.js
@@ -13,53 +13,19 @@ import { NativeRouter, Route, Link } from 'react-router-native';
 import { firebase } from '../config/store';
 
 import {styles, pallette} from '../styles';
+import SelectTemplate from './Create/SelectTemplate';
 
 export default class Join extends Component {
-    constructor(props) {
-        super(props);
-        this.state = {
-            selected: 0,
-            templates: [],
-        };
-    }
-
-    componentDidMount() {
-        firebase
-            .firestore()
-            .collection('templates')
-            .get()
-            .then(templates => {
-                templates.forEach(template => {
-                    this.setState({
-                        templates: [...this.state.templates, template],
-                    });
-                });
-            })
-            .catch(function(error) {
-                console.log('Error getting document:', error);
-            });
-    }
-
     render() {
-        let dataSource = new ListView.DataSource({
-            rowHasChanged: (r1, r2) => r1 !== r2,
-        }).cloneWithRows(this.state.templates);
-
-        var options =["Home","Savings","Car","GirlFriend"];
-
         return (
-            <View style={styles.content}>
-                <Text style={styles.header}>Create</Text>
-                <ListView dataSource={dataSource}
-                          style={{ flex: 1 }}
-                          enableEmptySections={true}
-                          renderRow={(template) => (
-                              <Link to="/discover">
-
-                                      <Text>{template.get('name')}</Text>
-
-                              </Link>)}/>
-            </View>);
+            <NativeRouter
+                initialIndex={0}
+                initialEntries={['/selectTemplate', '/gameSetup']}>
+                <View>
+                    <Route path="/selectTemplate" component={SelectTemplate}/>
+                    <Route path="/gameSetup" render={() => (<Text>Game Setup</Text>)}/>
+                </View>
+            </NativeRouter>);
     }
 }
 
