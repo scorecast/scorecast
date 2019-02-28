de32f3b1126c7b299df7d4ccc8a170d89b416cb2
SkipKayhil <skipkayhil@gmail.com>
2019-02-27 19:14:23 -0500

    add firebase, move Home

--- App.js
+++ App.js
@@ -1,36 +1,10 @@
 import React, { Component } from 'react';
-import {
-    Text,
-    View,
-    SafeAreaView,
-    StatusBar,
-    TouchableOpacity,
-} from 'react-native';
-
-import { NativeRouter, Route, Link } from 'react-router-native';
-import NavLink from './components/NavLink';
-
-import firebase from 'firebase';
-import 'firebase/firestore';
-var config = {
-    apiKey: 'AIzaSyBd8u4BvdZylqyztlgTToIi6D1a79k3emA',
-    authDomain: 'scorecast-5f168.firebaseapp.com',
-    databaseURL: 'https://scorecast-5f168.firebaseio.com',
-    projectId: 'scorecast-5f168',
-    storageBucket: 'scorecast-5f168.appspot.com',
-    messagingSenderId: '138861825009',
-};
-firebase.initializeApp(config);
-
+import { View, StatusBar } from 'react-native';
 import { Font, AppLoading } from 'expo';
-import Icon from 'react-native-vector-icons/FontAwesome';
+import { Provider } from 'react-redux';
+import Store from './config/store';
 
-import { styles, pallette } from './styles';
-import FontText from './components/Text';
-
-import Join from './components/Join';
-import Create from './components/Create';
-import Discover from './components/Discover';
+import Home from './components/Home';
 
 export default class App extends Component {
     state = {
@@ -46,102 +20,31 @@ export default class App extends Component {
 
         this.setState({ isReady: true });
 
-        await firebase
-            .firestore()
-            .collection('templates')
-            .get()
-            .then(templates => {
-                templates.forEach(template => {
-                    this.setState({
-                        templates: [...this.state.templates, template],
-                    });
-                });
-            })
-            .catch(function(error) {
-                console.log('Error getting document:', error);
-            });
+        // await firebase
+        //     .firestore()
+        //     .collection('templates')
+        //     .get()
+        //     .then(templates => {
+        //         templates.forEach(template => {
+        //             this.setState({
+        //                 templates: [...this.state.templates, template],
+        //             });
+        //         });
+        //     })
+        //     .catch(function(error) {
+        //         console.log('Error getting document:', error);
+        //     });
     }
 
     render() {
-        if (!this.state.isReady) {
-            return <AppLoading />;
-        }
-
-        return (
-            <View style={{ flex: 1, marginTop: StatusBar.currentHeight }}>
-                <NativeRouter
-                    initialIndex={1}
-                    initialEntries={['/create', '/join', '/discover']}
-                >
-                    <SafeAreaView style={styles.container}>
-                        <View style={[styles.navbar]}>
-                            <TouchableOpacity activeOpacity={0.5}>
-                                <Icon
-                                    name="arrow-left"
-                                    size={20}
-                                    color={pallette.darkgray}
-                                />
-                            </TouchableOpacity>
-                            <View style={styles.logo}>
-                                <FontText style={styles.logoText}>
-                                    Score
-                                </FontText>
-                                <FontText
-                                    style={[
-                                        styles.logoText,
-                                        { color: pallette.crimson },
-                                    ]}
-                                >
-                                    Cast
-                                </FontText>
-                            </View>
-                            <TouchableOpacity activeOpacity={0.5}>
-                                <Icon
-                                    name="user"
-                                    size={20}
-                                    color={pallette.darkgray}
-                                />
-                            </TouchableOpacity>
-                        </View>
-                        <View style={[styles.content]}>
-                            <Route
-                                path="/create"
-                                render={() => (
-                                    <Create rows={this.state.templates} />
-                                )}
-                            />
-                            <Route path="/join" component={Join} />
-                            <Route path="/discover" component={Discover} />
-                        </View>
-                        <View style={[styles.nav]}>
-                            <NavLink
-                                to="/create"
-                                component={TouchableOpacity}
-                                style={styles.navItem}
-                                activeStyle={styles.navItemActive}
-                                activeOpacity={0.5}
-                                iconName={'pencil'}
-                            />
-                            <NavLink
-                                to="/join"
-                                component={TouchableOpacity}
-                                style={styles.navItem}
-                                activeStyle={styles.navItemActive}
-                                activeOpacity={0.5}
-                                iconName={'rocket'}
-                            />
-                            <NavLink
-                                to="/discover"
-                                component={TouchableOpacity}
-                                style={styles.navItem}
-                                activeStyle={styles.navItemActive}
-                                activeOpacity={0.5}
-                                iconName={'history'}
-                            />
-                        </View>
-                    </SafeAreaView>
-                </NativeRouter>
-            </View>
+        return this.state.isReady ? (
+            <Provider store={Store}>
+                <View style={{ flex: 1, marginTop: StatusBar.currentHeight }}>
+                    <Home />
+                </View>
+            </Provider>
+        ) : (
+            <AppLoading />
         );
     }
 }
---
+++ components/Home.js
@@ -0,0 +1,90 @@
+import React from 'react';
+import {
+    Text,
+    View,
+    SafeAreaView,
+    StatusBar,
+    TouchableOpacity,
+} from 'react-native';
+
+import { NativeRouter, Route, Link } from 'react-router-native';
+import NavLink from './NavLink';
+
+import Icon from 'react-native-vector-icons/FontAwesome';
+
+import { styles, pallette } from '../styles';
+import FontText from './Text';
+
+import Join from './Join';
+import Create from './Create';
+import Discover from './Discover';
+
+const Home = props => {
+    console.log(props.firebase);
+
+    return (
+        <NativeRouter
+            initialIndex={1}
+            initialEntries={['/create', '/join', '/discover']}
+        >
+            <SafeAreaView style={styles.container}>
+                <View style={[styles.navbar]}>
+                    <TouchableOpacity activeOpacity={0.5}>
+                        <Icon
+                            name="arrow-left"
+                            size={20}
+                            color={pallette.darkgray}
+                        />
+                    </TouchableOpacity>
+                    <View style={styles.logo}>
+                        <FontText style={styles.logoText}>Score</FontText>
+                        <FontText
+                            style={[
+                                styles.logoText,
+                                { color: pallette.crimson },
+                            ]}
+                        >
+                            Cast
+                        </FontText>
+                    </View>
+                    <TouchableOpacity activeOpacity={0.5}>
+                        <Icon name="user" size={20} color={pallette.darkgray} />
+                    </TouchableOpacity>
+                </View>
+                <View style={[styles.content]}>
+                    <Route path="/create" render={() => <Create rows={[]} />} />
+                    <Route path="/join" component={Join} />
+                    <Route path="/discover" component={Discover} />
+                </View>
+                <View style={[styles.nav]}>
+                    <NavLink
+                        to="/create"
+                        component={TouchableOpacity}
+                        style={styles.navItem}
+                        activeStyle={styles.navItemActive}
+                        activeOpacity={0.5}
+                        iconName={'pencil'}
+                    />
+                    <NavLink
+                        to="/join"
+                        component={TouchableOpacity}
+                        style={styles.navItem}
+                        activeStyle={styles.navItemActive}
+                        activeOpacity={0.5}
+                        iconName={'rocket'}
+                    />
+                    <NavLink
+                        to="/discover"
+                        component={TouchableOpacity}
+                        style={styles.navItem}
+                        activeStyle={styles.navItemActive}
+                        activeOpacity={0.5}
+                        iconName={'history'}
+                    />
+                </View>
+            </SafeAreaView>
+        </NativeRouter>
+    );
+};
+
+export default Home;
